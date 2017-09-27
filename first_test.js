6184e0e12b9e5ea7382706084798610235cc3f2b
Yngve Høiseth <yhoiseth@gmail.com>
2017-09-27 18:14:30 +0200

    Add second app

--- first_test.js
+++ first_test.js
@@ -4,4 +4,6 @@ Feature('First');
 Scenario('test something', (I) => {
     I.amOnPage('/');
     I.see('Hello, World!');
+    I.moveCursorTo('#app-2');
+    I.seeNumberOfElements('[title^="You loaded this page on "]', 1);
 });
